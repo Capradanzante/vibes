65143a725a2b724336cbd1a2d0e855c611b4c2ed
cico <francescoaiello77@yahoo.com>
2025-04-09 12:36:58 +0200

    aggiunto docker

--- server/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchContent } = require('../controllers/searchController');
-
-router.get('/search', searchContent);
-
-module.exports = router; 
